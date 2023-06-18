da17c380c1d5c0f70624cf76006fc65f125f9f86
João Pedro Favarato <98183878+docafavarato@users.noreply.github.com>
2023-06-18 15:36:56 -0300

    Update Program.java

--- application/Program.java
+++ application/Program.java
@@ -10,7 +10,14 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		System.out.println("=== TEST 1 : seller findById ===");
-		Department dep = departmentDao.findById(2);
-		System.out.println(departmentDao.findAll());
+		System.out.println(sellerDao.findById(2));
+		System.out.println("=== TEST 2 : seller deleteById ===");
+		sellerDao.deleteById(2);
+		System.out.println("=== TEST 3 : seller update ===");
+		Seller s = new Seller(null, "test", "test@email", new Date(), 3400.00, new Department());
+		s.setEmail("new@email");
+		sellerDao.update(s);
+		System.out.println("=== TEST 4 : seller findAll ===");
+		System.out.println(sellerDao.findAll());
 	}
 }
